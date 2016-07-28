b6163c0297d1b173338e69a17236e213904dae29
Hamish Williams <tr4nsfuse@gmail.com>
2016-07-28 20:43:59 +1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Logo from './Logo';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-details">
+          <Logo color="#00BCD4"/>
+          <div className="App-header">
+            <h1 className="App-title">Gamestack</h1>
+            <div className="App-beta-tag">Beta</div>
+          </div>
+          <p className="App-subheading">coming soon</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
