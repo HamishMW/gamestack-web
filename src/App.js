5956c4bb89408054750469185b4e36b0ab937216
Hamish Williams <tr4nsfuse@gmail.com>
2017-06-13 23:07:03 +1000

    Add tracking code

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import Home from './screens/Home';
 import Auth from './screens/Auth';
 import Terms from './articles/Terms';
 import Privacy from './articles/Privacy';
+import ReactGA from 'react-ga';
 
 import {
   BrowserRouter as Router, Route, Link
 } from 'react-router-dom';
 
+ReactGA.initialize('UA-100980130-1');
+
+function fireTracking() {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
 const App = () => (
-  <Router>
+  <Router onUpdate={fireTracking}>
     <div>
       <Route exact path='/' component={Home}/>
       <Route path='/auth' component={Auth}/>
