0ed1e944f4c1766e19f0762ece7cb80b281b7f2b
Hamish Williams <tr4nsfuse@gmail.com>
2017-11-28 20:53:18 +1100

    Optimize assets

--- src/index.js
+++ src/index.js
@@ -4,8 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-render(
-  <App/>,
-  document.getElementById('root')
-);
+render(<App/>, document.getElementById('root'));
 registerServiceWorker();
