6879141abc0ef3071bb2e2c4c034e650c57769a7
Hamish Williams <tr4nsfuse@gmail.com>
2017-12-09 18:20:41 +1100

    Progressively load homepage image

---
+++ src/components/PhoneContainer.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Media } from '../utils/StyleUtils';
+
+const PhoneContainer = (props) => (
+  <PhoneContainerWrapper>
+    <PhoneContainerPhone>
+      <PhoneContainerScreen>
+        <PhoneContainerVideo
+          poster={props.frame}
+          autoPlay
+          muted
+          loop
+          playsInline
+        >
+          <source src={props.video} type="video/mp4" />
+        </PhoneContainerVideo>
+      </PhoneContainerScreen>
+    </PhoneContainerPhone>
+  </PhoneContainerWrapper>
+);
+
+const PhoneContainerWrapper = styled.div`
+  display: flex;
+  height: auto;
+  overflow: hidden;
+  padding: 0;
+  position: relative;
+  flex: 1 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+  overflow: visible;
+  padding-top: 40px;
+  padding-bottom: 80px;
+
+  @media (max-width: ${Media.tablet}) {
+    padding: 0;
+    justify-content: center;
+  }
+`;
+
+const PhoneContainerPhone = styled.div`
+  display: flex;
+  position: relative;
+  background: linear-gradient(#2E3747, #21252B);
+  justify-content: center;
+  border-radius: 54px;
+  max-width: 360px;
+  width: 100%;
+  height: auto;
+  box-shadow:
+    0 40px 50px ${props => props.theme.colorBlack(0.25)},
+    0 10px 20px rgba(0, 21, 64, 0.5),
+    0 2px 6px rgba(0, 21, 64, 0.15),
+    inset 0 -5px 38px ${props => props.theme.colorBackground(1)},
+    inset 0 4px 7px 1px ${props => props.theme.colorBlack(0.23)};
+
+  &:before {
+    content: '';
+    height: 6px;
+    width: 60px;
+    background: ${props => props.theme.colorBlack(0.3)};
+    border-radius: 3px;
+    position: absolute;
+    top: 28px;
+    left: 50%;
+    margin-left: -30px;
+  }
+
+  @media (max-width: ${Media.desktop}) {
+    max-width: 280px;
+    border-radius: 46px;
+  }
+`;
+
+const PhoneContainerScreen = styled.div`
+  flex: 1 0 auto;
+  padding: 60px 20px;
+  border-radius: 4px;
+  max-width: 100%;
+  display: flex;
+  align-items: flex-start;
+`;
+
+const PhoneContainerVideo = styled.video`
+  flex: 1 0 auto;
+  width: 100%;
+  height: auto;
+  box-shadow: 0 0 0 2px #21252B;
+  border-radius: 4px;
+  object-fit: cover;
+`;
+
+export default PhoneContainer;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,91 +1,114 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import Footer from '../components/Footer';
 import Container from '../components/Container';
 import MainHero from '../components/MainHero';
+import PhoneContainer from '../components/PhoneContainer';
 import Icon from '../utils/Icon';
 import { Media } from '../utils/StyleUtils';
 import HeroVideo from '../images/app-preview.mp4';
+import HeroVideoFrame from '../images/app-preview-frame.jpg';
+import BackgroundLargeBlur from '../images/background-large-blur.png';
 import BackgroundLarge from '../images/background-large.png';
 import BackgroundLarge2x from '../images/background-large@2x.png';
+import BackgroundMediumBlur from '../images/background-medium-blur.png';
 import BackgroundMedium from '../images/background-medium.png';
 import BackgroundMedium2x from '../images/background-medium@2x.png';
+import BackgroundSmallBlur from '../images/background-small-blur.png';
 import BackgroundSmall from '../images/background-small.png';
 import BackgroundSmall2x from '../images/background-small@2x.png';
-import HeroVideoFrame from '../images/app-preview-frame.jpg';
 
-const Home = () => (
-  <HomeContainer>
-    <MainHero
-      appName="Gamestack"
-      title="Finish your games"
-      description={
-        `Sync your Steam & Blizzard game libraries.
-        Track your progress, achievements, and time played.`
-      }
-    />
+class Home extends Component {
+  state = {
+    imageLoaded: false,
+  };
 
-    <PreviewSection>
-      <PhoneContainer>
-        <PhoneContainerPhone>
-          <PhoneContainerScreen>
-            <PhoneContainerVideo
-              autoPlay
-              muted
-              loop
-              playsInline
-              poster={HeroVideoFrame}
+  setImageLoaded = () => {
+    this.setState({imageLoaded: true});
+  }
+
+  render() {
+    const { imageLoaded } = this.state;
+
+    return (
+      <HomeContainer>
+        <MainHero
+          appName="Gamestack"
+          title="Finish your games"
+          description={
+            `Sync your Steam & Blizzard game libraries.
+            Track your progress, achievements, and time played.`
+          }
+        />
+
+        <PreviewSection>
+          <PhoneContainer video={HeroVideo} frame={HeroVideoFrame} />
+
+          <PreviewSectionDetail>
+            <PreviewSectionDetailRow>
+              <PreviewSectionDetailIcon icon="steam"/>
+              <PreviewSectionDetailIcon icon="blizzard" />
+            </PreviewSectionDetailRow>
+            <PreviewSectionDetailText>Currently supported</PreviewSectionDetailText>
+          </PreviewSectionDetail>
+
+          <PreviewSectionBackground>
+            <PreviewSectionPicture>
+              <source
+                srcSet={`${BackgroundLargeBlur}`}
+                media="(min-width: 1400px)"
+              />
+              <source
+                srcSet={`${BackgroundMediumBlur}`}
+                media="(min-width: 600px)"
+              />
+              <PreviewSectionImage
+                blur
+                src={BackgroundSmallBlur}
+                show={!imageLoaded}
+                srcSet={`${BackgroundSmallBlur}`}
+                alt="Gamestack – organize all your games in one place"
+              />
+            </PreviewSectionPicture>
+            <PreviewSectionPicture>
+              <source
+                srcSet={`${BackgroundLarge} 1x, ${BackgroundLarge2x} 2x`}
+                media="(min-width: 1400px)"
+              />
+              <source
+                srcSet={`${BackgroundMedium} 1x, ${BackgroundMedium2x} 2x`}
+                media="(min-width: 600px)"
+              />
+              <PreviewSectionImage
+                src={BackgroundSmall}
+                show={imageLoaded}
+                onLoad={this.setImageLoaded}
+                srcSet={`${BackgroundSmall} 1x, ${BackgroundSmall2x} 2x`}
+                alt="Gamestack – organize all your games in one place"
+              />
+            </PreviewSectionPicture>
+            <PreviewSectionAngle
+              top
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 100 100"
+              preserveAspectRatio="none"
             >
-              <source src={HeroVideo} type="video/mp4" />
-            </PhoneContainerVideo>
-          </PhoneContainerScreen>
-        </PhoneContainerPhone>
-      </PhoneContainer>
-
-      <PreviewSectionDetail>
-        <PreviewSectionDetailRow>
-          <PreviewSectionDetailIcon icon="steam"/>
-          <PreviewSectionDetailIcon icon="blizzard" />
-        </PreviewSectionDetailRow>
-        <PreviewSectionDetailText>Currently supported</PreviewSectionDetailText>
-      </PreviewSectionDetail>
-
-      <PreviewSectionBackground>
-        <PreviewSectionPicture>
-          <source
-            srcSet={`${BackgroundLarge} 1x, ${BackgroundLarge2x} 2x`}
-            media="(min-width: 1400px)"
-          />
-          <source
-            srcSet={`${BackgroundMedium} 1x, ${BackgroundMedium2x} 2x`}
-            media="(min-width: 600px)"
-          />
-          <PreviewSectionImage
-            src={BackgroundSmall}
-            srcSet={`${BackgroundSmall} 1x, ${BackgroundSmall2x} 2x`}
-            alt="Gamestack – organize all your games in one place"
-          />
-        </PreviewSectionPicture>
-        <PreviewSectionAngle
-          top
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 100 100"
-          preserveAspectRatio="none"
-        >
-          <polygon points="0 0, 100 0, 100 100"/>
-        </PreviewSectionAngle>
-        <PreviewSectionAngle
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 100 100"
-          preserveAspectRatio="none"
-        >
-          <polygon points="0 0, 0 100, 100 100"/>
-        </PreviewSectionAngle>
-      </PreviewSectionBackground>
-    </PreviewSection>
-    <HomeMobileFooter />
-  </HomeContainer>
-)
+              <polygon points="0 0, 100 0, 100 100"/>
+            </PreviewSectionAngle>
+            <PreviewSectionAngle
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 100 100"
+              preserveAspectRatio="none"
+            >
+              <polygon points="0 0, 0 100, 100 100"/>
+            </PreviewSectionAngle>
+          </PreviewSectionBackground>
+        </PreviewSection>
+        <HomeMobileFooter />
+      </HomeContainer>
+    )
+  }
+}
 
 const HomeContainer = styled(Container)`
   display: flex;
@@ -164,9 +187,15 @@ const PreviewSectionPicture = styled.picture`
 const PreviewSectionImage = styled.img`
   object-fit: cover;
   position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
   height: 100%;
   width: 100%;
-  opacity: 0.5;
+  opacity: ${props => props.show ? 0.5 : 0};
+  ${props => props.blur && 'filter: blur(10px);'}
+  transition: all 1s ease;
 `;
 
 const PreviewSectionAngle = styled.svg`
@@ -243,79 +272,6 @@ const PreviewSectionDetailText = styled.div`
   @media (max-width: ${Media.tablet}) {
     margin-bottom: 20px;
   }
-`;
-
-const PhoneContainer = styled.div`
-  display: flex;
-  height: auto;
-  overflow: hidden;
-  padding: 0;
-  position: relative;
-  flex: 1 0 auto;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 10;
-  overflow: visible;
-  padding-top: 40px;
-  padding-bottom: 80px;
-
-  @media (max-width: ${Media.tablet}) {
-    padding: 0;
-    justify-content: center;
-  }
-`;
-
-const PhoneContainerPhone = styled.div`
-  display: flex;
-  position: relative;
-  background: linear-gradient(#2E3747, #21252B);
-  justify-content: center;
-  border-radius: 54px;
-  max-width: 360px;
-  width: 100%;
-  height: auto;
-  box-shadow:
-    0 40px 50px ${props => props.theme.colorBlack(0.25)},
-    0 10px 20px rgba(0, 21, 64, 0.5),
-    0 2px 6px rgba(0, 21, 64, 0.15),
-    inset 0 -5px 38px ${props => props.theme.colorBackground(1)},
-    inset 0 4px 7px 1px ${props => props.theme.colorBlack(0.23)};
-
-  &:before {
-    content: '';
-    height: 6px;
-    width: 60px;
-    background: ${props => props.theme.colorBlack(0.3)};
-    border-radius: 3px;
-    position: absolute;
-    top: 28px;
-    left: 50%;
-    margin-left: -30px;
-  }
-
-  @media (max-width: ${Media.desktop}) {
-    max-width: 280px;
-    border-radius: 46px;
-  }
-`;
-
-const PhoneContainerScreen = styled.div`
-  flex: 1 0 auto;
-  padding: 60px 20px;
-  border-radius: 4px;
-  max-width: 100%;
-  display: flex;
-  align-items: flex-start;
-`;
-
-const PhoneContainerVideo = styled.video`
-  flex: 1 0 auto;
-  width: 100%;
-  height: auto;
-  box-shadow: 0 0 0 2px #21252B;
-  border-radius: 4px;
-  object-fit: cover;
 `;
 
 export default Home;
